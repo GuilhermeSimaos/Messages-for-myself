07a29fd1f737e37a237630982cf605f906f77efb
GuilhermeSimaos <guilhermesimaogh@outlook.com>
2023-07-08 12:47:32 -0300

    Solved cors policy for get operations

--- index.js
+++ index.js
@@ -11,64 +11,76 @@ messageForm.addEventListener("input", () => {
 const result = document.getElementById("status");
 const postMessage = document.getElementById("post");
 postMessage.addEventListener("click", () => {
-  if(messageForm.value){
+  if (messageForm.value) {
     messageForm.classList.remove("is-invalid");
     var json = {
-      "message": messageForm.value
+      message: messageForm.value,
     };
-  
-    axios.post(SERVER_URL+"/post/message", {
-      headers:{
-        'Access-Control-Allow-Origin': 'https://messages-for-myself.vercel.app'
-      },
-      json
-    }).then((response) => {
-      console.log(response.data);
-      if (response.status == 200) {
-        result.classList.add("text-success");
-        result.classList.remove("text-danger");
-      } else {
-        result.classList.add("text-danger");
-        result.classList.remove("text-success");
-      }
-      result.innerText = response.statusText;
-    }).catch((error) => {
-      console.log(error);
-    });
-    setTimeout(()=>{
+
+    axios
+      .post(SERVER_URL + "/post/message", json)
+      .then((response) => {
+        console.log(response.data);
+        if (response.status == 200) {
+          result.classList.add("text-success");
+          result.classList.remove("text-danger");
+        } else {
+          result.classList.add("text-danger");
+          result.classList.remove("text-success");
+        }
+        result.innerText = response.statusText;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setTimeout(() => {
       result.innerText = "";
-    },5000);
-  }else{
+    }, 5000);
+  } else {
     messageForm.classList.add("is-invalid");
   }
 });
 
 const tableMessage = document.getElementById("tableMessage");
 const random = document.getElementById("getRandom");
-random.addEventListener("click",()=>{
-  axios.get(SERVER_URL+"/get/random-message").then((response)=>{
-    // console.log(response.data);
-    const span = document.createElement("span");
-    span.innerText = response.data;
-    tableMessage.replaceChildren(span);
-  }).catch((error)=>{
-    console.log(error);
-  });
+random.addEventListener("click", () => {
+  axios
+    .get(SERVER_URL + "/get/random-message", {
+      headers: {
+        "Access-Control-Allow-Origin": "https://messages-for-myself.vercel.app",
+      },
+    })
+    .then((response) => {
+      // console.log(response.data);
+      const span = document.createElement("span");
+      span.innerText = response.data;
+      tableMessage.replaceChildren(span);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 const all = document.getElementById("getAll");
-all.addEventListener("click",()=>{
-  axios.get(SERVER_URL+"/get/all-messages").then((response)=>{
-    // console.log(response.data);
-    const span = document.createElement("span");
-    span.innerText = response.data;
-    tableMessage.replaceChildren(span);
-  }).catch((error)=>{
-    console.log(error);
-  });
+all.addEventListener("click", () => {
+  axios
+    .get(SERVER_URL + "/get/all-messages", {
+      headers: {
+        "Access-Control-Allow-Origin": "https://messages-for-myself.vercel.app",
+      },
+    })
+    .then((response) => {
+      // console.log(response.data);
+      const span = document.createElement("span");
+      span.innerText = response.data;
+      tableMessage.replaceChildren(span);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 const clear = document.getElementById("clearRandom");
-clear.addEventListener("click",()=>{
+clear.addEventListener("click", () => {
   tableMessage.innerHTML = "";
 });
